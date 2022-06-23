82be851f9ea4e7cc19618e9c074743112c353f26
Ciro Batista da Silva <cirobatista.cbs2020ti@outlook.com>
2022-06-23 12:06:43 -0300

    Update App.tsx
    Trying to requisition with Apollo.

--- 2-Ignite Lab - The power of React JS/event-platform/src/App.tsx
+++ 2-Ignite Lab - The power of React JS/event-platform/src/App.tsx
@@ -1,6 +1,24 @@
 //Essa função é um componente chamado no 'main'.
+import { gql } from "@apollo/client"
+import { useEffect } from "react"
+import { client } from "./lib/apollo"
+
+const GET_LESSONS_QUERY = gql '
+query {
+  lessons {
+    id
+    title
+  }
+}
+'
+
 //This function is a component called in 'main'.
 function App() {
+  useEffect(() => {
+    client.query({
+      query: GET_LESSONS_QUERY,
+    })
+  }, [])
 
 //Tudo que é exibido no retorno da função é o arquivo Html mostrado na página.
 //All that is displayed in the function return is the Html file shown on the page.
