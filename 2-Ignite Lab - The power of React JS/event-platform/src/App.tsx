e4e878dc61ed5a63a33fc439ea923cad02117217
Ciro Batista da Silva <cirobatista.cbs2020ti@outlook.com>
2022-06-29 12:43:49 -0300

    Update-Css
    Beginning of the construction of the graphical interface.

--- 2-Ignite Lab - The power of React JS/event-platform/src/App.tsx
+++ 2-Ignite Lab - The power of React JS/event-platform/src/App.tsx
@@ -1,3 +1,9 @@
+import { Header } from "./components/Header"
+import { Lesson } from "./components/Lesson"
+import { Sidebar } from "./components/Sidebar"
+import { Video } from "./components/Video"
+import { Event } from "./pages/Event"
+
 //Essa função é um componente chamado no 'main'.
 function App() {
   
@@ -6,7 +12,7 @@ function App() {
 //All that is displayed in the function return is the Html file shown on the page.
   return (
     <div>
-
+      < Event />
     </div>
   )
 }
